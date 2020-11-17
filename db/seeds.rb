28a20a70c579266fbb9353badc1d6585c5cf352f
Sara Ward <saraward@MacBook-Pro-6.local>
2020-11-17 15:30:39 -0500

    new repo

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Song.create([
+    {title: "Iceland", artist: "Petite Biscuit", time: 4.42},
+    {title: "Mockingbird", artist: "Ruston Kelly", time: 4.37},
+    {title: "Surefire", artist: "Wilderado", time: 4.00},
+    {title: "Honeybee", artist: "The Head and the Heart", time: 3.16},
+    {title: "Green Mountain State", artist: "Trevor Hall", time: 4.35},
+    {title: "Midnight Sky", artist: "Petite Biscuit", time: 4.38},
+    {title: "Lay Down", artist: "Son Little", time: 3.48},
+    {title: "Alaska", artist: "Maggie Rogers", time: 3.09},
+    {title: "Free Spirit", artist: "Khalid", time: 3.02},
+    {title: "Pyro", artist: "Kings of Leon", time: 4.11},
+    {title: "The Halocline", artist: "Hippo Campus", time: 6.23},
+])
